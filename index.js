10e99000c2c67fa07c4b3a5b8160275c60f4d390
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-29 16:10:40 -0500

    Bored bot api button

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ btn.addEventListener('click', () => {
   fetch("https://apis.scrimba.com/bored/api/activity")
   .then(response => response.json())
   .then(data => {
-      console.log(data)
       document.getElementById("activity-name").textContent = data.activity
   })
 });
