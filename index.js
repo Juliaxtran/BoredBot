41a65b5dad7b76ee46b6adc784d9e88d8c4c7221
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-29 15:39:24 -0500

    Bored Bot HTML

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        document.getElementById("activity-name").textContent = data.activity
-    })
+// fetch("https://apis.scrimba.com/bored/api/activity")
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data)
+//         document.getElementById("activity-name").textContent = data.activity
+//     })
