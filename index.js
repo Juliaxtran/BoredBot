3e4c62d885a511e9cbfb3af5b49bfd4703a3169b
Julia Tran <julia.kim.tran@hotmail.com>
2022-11-29 16:08:43 -0500

    Add button functionality

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const btn = document.getElementById('btn');
 
+btn.addEventListener('click', () => {
+  console.log('clicked');
+  fetch("https://apis.scrimba.com/bored/api/activity")
+  .then(response => response.json())
+  .then(data => {
+      console.log(data)
+      document.getElementById("activity-name").textContent = data.activity
+  })
+});
 
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
